7904f57b24454e0b930b66e542cb03d659465e14
AnshGadoya <anshgadoya009@gmail.com>
2025-08-13 15:29:05 +0530

    commit-demo-ansh

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -7,8 +7,8 @@ function AdminDashboard() {
             <div className="container">
                 <div className="row m-10 p-5">
                     <CardView  title="Dashboard 123"  description="Dashboard" click="/dashboard"/>
-                    {/*<CardView  title="service" description="Service" click="/service"/>*/}
-                    {/*<CardView  title="Home" description="Home" click="/home"/>*/}
+                    <CardView  title="service" description="Service" click="/service"/>
+                    <CardView  title="Home" description="Home" click="/home"/>
                     {/*<CardView  title="About" description="About" click="/about"/>*/}
                 </div>
             </div>
