f526d310b0fada090d3fec9ffd7e295883902e17
AnshGadoya <anshgadoya009@gmail.com>
2025-08-20 00:10:27 +0530

    7 commit-ansh

---
+++ src/pages/admin/AddFacility.jsx
@@ -0,0 +1,10 @@
+
+function AddFacility(){
+    return(
+        <div>
+            <label className="col-md-6"> Add Facility</label>
+            <select name="" id=""></select>
+        </div>
+    );
+}
+export default AddFacility
