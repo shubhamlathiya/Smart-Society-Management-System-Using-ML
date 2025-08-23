2869229cfc1cbf5e34d9c6b6c604d0dea0673294
AnshGadoya <anshgadoya009@gmail.com>
2025-08-23 23:31:10 +0530

    9 commit-ansh

--- src/utils/constants/apiConstants.js
+++
@@ -1,8 +0,0 @@
-const API_BASE_URL = 'http://127.0.0.1:5000/';
-
-const HOUSING_POST = API_BASE_URL + "housing/";
-const HOUSING_GET = API_BASE_URL + "housing/";
-
-const COMPLAINT_POST = API_BASE_URL + "complaint/";
-
-export {HOUSING_POST, HOUSING_GET, COMPLAINT_POST};
--- src/utils/constants/appConstants.js
+++
@@ -1,5 +0,0 @@
-const unitTypes = ['1BHK', '2BHK', '3BHK', '4BHK', 'Penthouse', 'Studio'];
-const facilityTypes =['Hall','Theater','Garden','Clubhouse']
-
-export {unitTypes} ;
-export {facilityTypes};
